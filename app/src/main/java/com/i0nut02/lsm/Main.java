4154ef2d7cfa24f2181b7860a40f11aad8258efd
I0nut02 <ionu.radu02@gmail.com>
2025-04-05 19:13:50 +0200

    add project config

---
+++ app/src/main/java/com/i0nut02/lsm/Main.java
@@ -0,0 +1,11 @@
+package com.i0nut02.lsm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public String getGreeting() {
+        return "I'm here";
+    }
+}
